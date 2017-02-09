27ba14c5d968d94c6e5d3bf6fbe7826798a6e167
Calvin <cliumay9@gmail.com>
2017-02-08 16:56:14 -0800

    Updated on finished multiple linear regression

--- Multiple Linear Regression/MultipleLinearRegression.py
+++ Multiple Linear Regression/MultipleLinearRegression.py
@@ -43,7 +43,7 @@ regressor = LinearRegression()
 regressor.fit(X_train, Y_train)
 
 # Predicting the Test set results with the regressor(The Mult Linear Regression Model)
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(X_test) 
 
 # Optimize Mult Linear Regression with Backward Elimination
 # Remove lower statistic signficant variables
@@ -55,4 +55,45 @@ import statsmodels.formula.api as sm
 # axis is 1 because we wanted to add a COLUMN. if one needs to add a row axis =0
 
 X = np.append(arr= np.ones((50,1)).astype(int), values = X, axis=1)
+
+# Start backwardelimination After adding one's
+# Create X_opt matrix for the optimal matrix for high impact independent variables
+X_opt = X[:,[0,1,2,3,4,5]]
+# Let's fit the model to the X_opt by creating new regressor from statsmodel
+# Fitting the full model with all possible predictors
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit() 
+# Note: Ordinary Least Square regressor is created from sm library
+# Showed a table with how statistical signficant each variable is.
+regressor_OLS.summary() 
+
+# Remove X2, that is dummy variable for New York X[:,2]
+# Because X2 P value is 99% >> 5% - our signficant level
+X_opt = X[:,[0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary() 
+
+# Remove X1  (X[:,1]), that is the dummy variable for Florida, because P-value is 94%>> SL=5%
+X_opt = X[:,[0,3,4,5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary() 
+
+# Remvove X2, X[:,4], that is the administration cost
+# because its p-value is 0.602>0.05(our sl)
+X_opt = X[:,[0,3,5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary() 
+
+# Remove X[:,5] because 0.06>0.05 our sl.
+# There are ways to improve our model that will change our signficance level
+# But for this model we follow strictly with backwd elimination
+
+X_opt = X[:,[0,3]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary() 
+
+# Only R& D spent is left
+# This example happends to be a linear regression
+
+
+
 
