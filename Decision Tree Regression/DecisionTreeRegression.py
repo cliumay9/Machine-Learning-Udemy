8ae69e8922a82de80ed093479706902bb74bc9c2
Calvin <cliumay9@gmail.com>
2017-02-23 00:24:50 -0800

    Updated on Finished Decision Tree Regression

--- Decision Tree Regression/DecisionTreeRegression.py
+++ Decision Tree Regression/DecisionTreeRegression.py
@@ -1,3 +1,43 @@
 # Decission Tree Regression
+# Regression Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('PositionSalaries.csv')
+X = dataset.iloc[:, 1:2].values
+Y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_Y = StandardScaler()
+Y_train = sc_Y.fit_transform(Y_train)"""
+
+# Fitting the Decision Tree Regression Model to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0) #Put random_state to have consistency
+regressor.fit(X, Y)
 
 
+# Predicting a new result
+Y_pred = regressor.predict(6.5)
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
