2fc23bd80cd1674c4421d9027bdc91b0b19ac391
Calvin <cliumay9@gmail.com>
2017-03-17 00:10:38 -0700

    Updates on NLP done.

--- Natural Language PRocessing/NLP.py
+++ Natural Language PRocessing/NLP.py
@@ -6,4 +6,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Import the dataset
-dataset =
+dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter = '\t', 
+                      quoting =3 )
+
+# Cleaning the texts for our simple bag-of-words model
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+review = re.sub('[^a-zA-Z]', ' ',dataset['Review'][0]) # Keeping a-z and A-Z
+review =  review.lower()    #make everything to lowercase
+review = review.split()
+review = [word for word in review if not word in set(stopwords.words('english'))]
+# Its faster for python to go through words in set(*) than list
+
+for review in dataset['Review']:
+    for i in range(dataset.shape[0]):
+        dataset[review][i]
+
