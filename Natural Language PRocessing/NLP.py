3d28187d8e07ea25616612e210e35759a86683f1
Calvin <cliumay9@gmail.com>
2017-03-18 11:42:25 -0700

    Building simple bag of words model with NLP

--- Natural Language PRocessing/NLP.py
+++ Natural Language PRocessing/NLP.py
@@ -7,20 +7,24 @@ import pandas as pd
 
 # Import the dataset
 dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter = '\t', 
-                      quoting =3 )
-
+                      quoting =3 ) # Get rid of quotes
+# Data Preprocessing
 # Cleaning the texts for our simple bag-of-words model
 import re
 import nltk
-nltk.download('stopwords')
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
-review = re.sub('[^a-zA-Z]', ' ',dataset['Review'][0]) # Keeping a-z and A-Z
-review =  review.lower()    #make everything to lowercase
-review = review.split()
-review = [word for word in review if not word in set(stopwords.words('english'))]
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(dataset.shape[0]):
+    review = re.sub('[^a-zA-Z]', ' ',dataset['Review'][i]) # Keeping a-z and A-Z
+    review =  review.lower()    #make everything to lowercase
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
 # Its faster for python to go through words in set(*) than list
 
-for review in dataset['Review']:
-    for i in range(dataset.shape[0]):
-        dataset[review][i]
+
 
