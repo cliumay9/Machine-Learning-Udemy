989cba09a32e14c3ae3c615c9535b9822da25124
Calvin <cliumay9@gmail.com>
2017-03-22 13:42:19 -0700

    Updates on finished PCA and LDA

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -1,1 +1,100 @@
-# PCA
+# Principal Component Analysis (PCA) - feature extraction
+"""PCA - Explain the most about variance of the dataset regardless of the dependent variable
+  Bkwd elimination PCA; Frwd Selection LDA; Bidirectional Elimination Kernel PCA
+  Score Comparison
+  Data from UCI Machine Learning Repo
+  Dependent Variables: Customer Segments
+  Predict which customer segments does each new wine recommended to
+  Without dimensional reduction, we need 13 dimension to plot the data
+  PCA > unsupervised model
+"""
+# Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+# prepare the dataset with Matrix X, Y indendent and dependent variables
+dataset = pd.read_csv("Wine.csv")
+
+# Matrix the X and Y, independent and dependent variables
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,-1].values
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, random_state = 0)
+
+#Feature Scaling (MUST for PCA, LDA)
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+## End of Data Preprocessing
+
+### Applying PCA  ###
+"""
+Import class, create object of this class, 
+and apply fit_transform and transform from 
+the object on the training set and test set
+"""
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2) 
+# use None to see which components has the highest explained variance ratio
+# with trained pac, .explained_variance_ratio
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+# Now X_train and X_test have 2 indepednet variables with highest explained variance ratio
+
+## Fitting Logistic Regressions to the TrainingSet
+# Linear = Logistic regresison
+# Class is capital letters while function is not
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0) # setting random_state = 0 to provide consistency
+classifier.fit(X_train, Y_train)
+
+## Predict Test set result with classifier
+y_pred = classifier.predict(X_test)
+
+## Building Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+
+## Visualizing the Training set results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step= 0.01),
+                                np.arange(start=X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step= 0.01))
+plt.contourf(X1,X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+                                                alpha =.75, cmap=ListedColormap(('red','green', 'blue')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set== j,0],X_set[Y_set ==j,1],
+                c= ListedColormap(('red','green', 'blue'))(i), label = j)
+plt.title('Logistic Regression, PCA (Training Set)')
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.legend()
+plt.show()
+# Note: principal components are max(variance)
+
+## Visualizing the test set results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step= 0.01),
+                                np.arange(start=X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step= 0.01))
+plt.contourf(X1,X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+                                                alpha =.75, cmap=ListedColormap(('red','green','blue')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set== j,0],X_set[Y_set ==j,1],
+                c= ListedColormap(('red','green', 'blue'))(i), label = j)
+plt.title('Logistic Regression, PCA (Test Set)')
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend()
+plt.show()
